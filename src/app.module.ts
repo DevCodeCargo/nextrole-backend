62accc3cdda941c6330139d6dde6ef0375030289
Nishant Badlani <nishantbadlani2@gmail.com>
2026-05-24 11:27:48 +0530

    add required npm packages and app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,68 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
+import { InternalTestModule } from './modules/internal-test/internal-test.module';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+
+import { GlobalAuthGuard } from './common/auth/auth.guard';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import type { StringValue } from 'ms';
+import { AuthModule } from './common/auth/auth.module';
+import { GlobalExceptionFilter } from './common/exceptions/global-exception.filter';
+import { StartupModule } from './common/startup/startup.module';
+import { SeederService } from './common/startup/seeder.service';
+import { DomainsModule } from './modules/domains.module';
+import { AppValidationPipe } from './common/pipes/validation.pipe';
+import { SuccessResponseInterceptor } from './common/helper/success-response.interceptor';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PG_HOST: Joi.string().required(),
+        PG_PORT: Joi.number().required(),
+        PG_USER: Joi.string().required(),
+        PG_PASSWORD: Joi.string().required(),
+        PG_DATABASE: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string()
+          .pattern(/^\d+[smhdwy]$/)
+          .default('15m'),
+        //PG_SSL: Joi.boolean().required()
+      }),
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_EXPIRES_IN') as StringValue,
+        },
+      }),
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GlobalAuthGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: AppValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: SuccessResponseInterceptor,
+    },
+    SeederService,
+  ]
 })
-export class AppModule {}
+
+export class AppModule { }
